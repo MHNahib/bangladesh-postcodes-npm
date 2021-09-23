8ee13ee0f23e73010713134ff80fc8fe229207a2
MHNahib <MHNahib@users.noreply.github.com>
2021-09-23 17:27:34 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const details = require("./lib/process");
+
+module.exports.postCodeObject = details.getPostcodeObject;
+module.exports.postOfficeObject = details.getPostOfficeObject;
+module.exports.postCode = details.getPostcode;
+module.exports.postOffice = details.getPostOffice;
